7c2df0e91592a3a3b4e77857ab73160c483d3468
Azizan Nur Rohman <zhiz.nevermind@gmail.com>
2022-06-10 05:55:24 +0700

    Watch Properties

--- elementary/assets/js/app.js
+++ elementary/assets/js/app.js
@@ -1,81 +1,98 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-      // show: true, //way data is boolean
-      //show: false, //way data is opposite in html use !
-      //show: 'ready', //other way if data is not boolean
-      show:true,
-      message: 'Hello World! Pasti bisa',
-      message2: 'Hello Vue!'
-    }
-  })
+// let app = new Vue({
+//     el: '#app',
+//     data: {
+//       // show: true, //way data is boolean
+//       //show: false, //way data is opposite in html use !
+//       //show: 'ready', //other way if data is not boolean
+//       show:true,
+//       message: 'Hello World! Pasti bisa',
+//       message2: 'Hello Vue!'
+//     }
+//   })
 
-  let app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  })
+//   let app2 = new Vue({
+//     el: '#app-2',
+//     data: {
+//       message: 'You loaded this page on ' + new Date().toLocaleString()
+//     }
+//   })
 
-  let app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
+//   let app3 = new Vue({
+//     el: '#app-3',
+//     data: {
+//       seen: true
+//     }
+//   })
 
-  let app4 = new Vue({
-    el: '#app-4',
-    data: {
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' }
-      ]
-    }
-  })
+//   let app4 = new Vue({
+//     el: '#app-4',
+//     data: {
+//       todos: [
+//         { text: 'Learn JavaScript' },
+//         { text: 'Learn Vue' },
+//         { text: 'Build something awesome' }
+//       ]
+//     }
+//   })
 
-  let appComputed = new Vue({
-    el: '#app-computed',
-    data: {
-      int1: 1,
-      int2: 4
-    },
-    computed: {
-      sum: function(){
-        return this.int1+this.int2;
-      },
-      umur: function(){
-        let umurKata = '';
-        if(this.int1 == 2){
-          umurKata = 'umur satu bro';
-        }else{
-          umurKata = 'umur 2 bro' ;
-        }
-        return umurKata;
-      },
-      kelas: function(){
-        let kelas = 'belum bisa masuk ya';
-        if(this.sum <= 3){
-          kelas = 'ini masuk kelas batita dan masuk balita';
-        }else{
-          kelas = 'udah bisa masuk sekolah ya';
-        }
-        return kelas;
-      }
-    }
-  })
+//   let appComputed = new Vue({
+//     el: '#app-computed',
+//     data: {
+//       int1: 1,
+//       int2: 4
+//     },
+//     computed: {
+//       sum: function(){
+//         return this.int1+this.int2;
+//       },
+//       umur: function(){
+//         let umurKata = '';
+//         if(this.int1 == 2){
+//           umurKata = 'umur satu bro';
+//         }else{
+//           umurKata = 'umur 2 bro' ;
+//         }
+//         return umurKata;
+//       },
+//       kelas: function(){
+//         let kelas = 'belum bisa masuk ya';
+//         if(this.sum <= 3){
+//           kelas = 'ini masuk kelas batita dan masuk balita';
+//         }else{
+//           kelas = 'udah bisa masuk sekolah ya';
+//         }
+//         return kelas;
+//       }
+//     }
+//   })
 
   let appMethods = new Vue({
     el: '#app-methods',
     data: {
       int1: 1,
       int2: 2,
-      result: null
+      result: null,
+      kilometers: 0,
+      meters: 0
+    },
+    computed: {
+      sum: function(){
+        return this.int1+this.int2;
+      }
     },
     methods:{
       sumProses: function(int3){
         return this.result = this.int1 + this.int2 + int3;
+      }
+    },
+    watch:{
+      kilometers: function(val){
+        this.kilometers = val;
+        this.meters = val * 1000;
+      },
+      meters: function(val){
+        this.kilometers = val / 1000;
+        this.meters = val;
       }
     }
   })
